95a87375365a2ceea22ba572d06083b7a08d94f4
Dinhhai2002 <88237144+Dinhhai2002@users.noreply.github.com>
2025-03-17 22:15:51 +0700

    feature : add api authentication

--- src/main/java/com/shoes/webshoes/controller/JwtAuthenticationController.java
+++ src/main/java/com/shoes/webshoes/controller/JwtAuthenticationController.java
@@ -10,19 +10,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.shoes.webshoes.common.enums.OtpEnum;
 import com.shoes.webshoes.common.utils.HttpService;
 import com.shoes.webshoes.common.utils.Pagination;
 import com.shoes.webshoes.common.utils.StringErrorValue;
 import com.shoes.webshoes.common.utils.Utils;
+import com.shoes.webshoes.entity.Banner;
+import com.shoes.webshoes.entity.Brand;
 import com.shoes.webshoes.entity.Cart;
+import com.shoes.webshoes.entity.Category;
+import com.shoes.webshoes.entity.Product;
 import com.shoes.webshoes.entity.UserRegister;
 import com.shoes.webshoes.entity.Users;
+import com.shoes.webshoes.entity.Size;
+import com.shoes.webshoes.entity.Materials;
+import com.shoes.webshoes.entity.Color;
+import com.shoes.webshoes.entity.ProductDetail;
 import com.shoes.webshoes.model.StoreProcedureListResult;
 import com.shoes.webshoes.request.CRUDUserRequest;
 import com.shoes.webshoes.request.ConfirmOtpRequest;
@@ -31,11 +42,28 @@ import com.shoes.webshoes.request.JwtRequest;
 import com.shoes.webshoes.request.OTPRegisterUserRequest;
 import com.shoes.webshoes.request.OTPRequest;
 import com.shoes.webshoes.request.ResetPasswordRequest;
+import com.shoes.webshoes.response.BannerResponse;
+import com.shoes.webshoes.response.BaseListDataResponse;
 import com.shoes.webshoes.response.BaseResponse;
+import com.shoes.webshoes.response.BrandResponse;
+import com.shoes.webshoes.response.CategoryResponse;
 import com.shoes.webshoes.response.JwtResponse;
+import com.shoes.webshoes.response.ProductResponse;
 import com.shoes.webshoes.response.UserResponse;
+import com.shoes.webshoes.response.SizeResponse;
+import com.shoes.webshoes.response.MaterialsResponse;
+import com.shoes.webshoes.response.ColorResponse;
+import com.shoes.webshoes.response.ProductDetailResponse;
+import com.shoes.webshoes.service.BannerService;
+import com.shoes.webshoes.service.BrandService;
 import com.shoes.webshoes.service.CartService;
+import com.shoes.webshoes.service.CategoryService;
+import com.shoes.webshoes.service.ProductService;
 import com.shoes.webshoes.service.UserRegisterService;
+import com.shoes.webshoes.service.SizeService;
+import com.shoes.webshoes.service.MaterialsService;
+import com.shoes.webshoes.service.ColorService;
+import com.shoes.webshoes.service.ProductDetailService;
 
 
 @RestController
@@ -46,6 +74,30 @@ public class JwtAuthenticationController extends BaseController {
     
     @Autowired
     public CartService cartService;
+    
+    @Autowired
+    private BannerService bannerService;
+    
+    @Autowired
+    private CategoryService categoryService;
+    
+    @Autowired
+    private BrandService brandService;
+    
+    @Autowired
+    private ProductService productService;
+    
+    @Autowired
+    private SizeService sizeService;
+    
+    @Autowired
+    private MaterialsService materialsService;
+    
+    @Autowired
+    private ColorService colorService;
+    
+    @Autowired
+    private ProductDetailService productDetailService;
     
     @PostMapping("/login")
     public ResponseEntity<BaseResponse<JwtResponse>> createAuthenticationToken(@RequestBody JwtRequest wrapper)
@@ -329,6 +381,174 @@ public class JwtAuthenticationController extends BaseController {
                 applicationProperties.getBaseUrl());
         registerUser.setIsLogin(1);
         response.setData(new JwtResponse(token));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/banners")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<BannerResponse>>> getBanners(
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<BannerResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<Banner> listBanner = bannerService.spGListBanner(keySearch, status, pagination);
+
+        BaseListDataResponse<BannerResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new BannerResponse().mapToList(listBanner.getResult()));
+        listData.setTotalRecord(listBanner.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<CategoryResponse>>> getCategories(
+            @RequestParam(name = "parent_id", required = false, defaultValue = "-1") int parentId,
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<CategoryResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<Category> listCategory = categoryService.spGListCategory(
+                parentId, keySearch, status, pagination);
+
+        BaseListDataResponse<CategoryResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new CategoryResponse().mapToList(listCategory.getResult()));
+        listData.setTotalRecord(listCategory.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/brands")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<BrandResponse>>> getBrands(
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<BrandResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<Brand> listBrand = brandService.spGListBrand(keySearch, status, pagination);
+
+        BaseListDataResponse<BrandResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new BrandResponse().mapToList(listBrand.getResult()));
+        listData.setTotalRecord(listBrand.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<ProductResponse>>> getProducts(
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<ProductResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<Product> listProduct = productService.spGListProduct(keySearch, status, pagination);
+
+        BaseListDataResponse<ProductResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new ProductResponse().mapToList(listProduct.getResult()));
+        listData.setTotalRecord(listProduct.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/sizes")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<SizeResponse>>> getSizes(
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<SizeResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<Size> listSize = sizeService.spGListSize(keySearch, status, pagination);
+
+        BaseListDataResponse<SizeResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new SizeResponse().mapToList(listSize.getResult()));
+        listData.setTotalRecord(listSize.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/materials")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<MaterialsResponse>>> getMaterials(
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<MaterialsResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<Materials> listMaterials = materialsService.spGListMaterials(keySearch, status, pagination);
+
+        BaseListDataResponse<MaterialsResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new MaterialsResponse().mapToList(listMaterials.getResult()));
+        listData.setTotalRecord(listMaterials.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/colors")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<ColorResponse>>> getColors(
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<ColorResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<Color> listColor = colorService.spGListColor(keySearch, status, pagination);
+
+        BaseListDataResponse<ColorResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new ColorResponse().mapToList(listColor.getResult()));
+        listData.setTotalRecord(listColor.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/product-details")
+    public ResponseEntity<BaseResponse<BaseListDataResponse<ProductDetailResponse>>> getProductDetails(
+            @RequestParam(name = "product_id", required = false, defaultValue = "-1") int productId,
+            @RequestParam(name = "color_id", required = false, defaultValue = "-1") int colorId,
+            @RequestParam(name = "size_id", required = false, defaultValue = "-1") int sizeId,
+            @RequestParam(name = "material_id", required = false, defaultValue = "-1") int materialId,
+            @RequestParam(name = "brand_id", required = false, defaultValue = "-1") int brandId,
+            @RequestParam(name = "category_id", required = false, defaultValue = "-1") int categoryId,
+            @RequestParam(name = "key_search", required = false, defaultValue = "") String keySearch,
+            @RequestParam(name = "status", required = false, defaultValue = "-1") int status,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) throws Exception {
+        BaseResponse<BaseListDataResponse<ProductDetailResponse>> response = new BaseResponse<>();
+        Pagination pagination = new Pagination(page, limit);
+        StoreProcedureListResult<ProductDetail> listProductDetail = productDetailService.spGListProductDetail(
+            productId, colorId, sizeId, materialId, brandId, categoryId, keySearch, status, pagination);
+
+        BaseListDataResponse<ProductDetailResponse> listData = new BaseListDataResponse<>();
+        listData.setList(new ProductDetailResponse().mapToList(listProductDetail.getResult()));
+        listData.setTotalRecord(listProductDetail.getTotalRecord());
+
+        response.setData(listData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<BaseResponse<ProductResponse>> getProductById(@PathVariable("id") int id) throws Exception {
+        BaseResponse<ProductResponse> response = new BaseResponse<>();
+        Product product = productService.findOne(id);
+
+        if (product == null) {
+            response.setStatus(HttpStatus.BAD_REQUEST);
+            response.setMessageError(StringErrorValue.PRODUCT_NOT_FOUND);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        response.setData(new ProductResponse(product));
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
